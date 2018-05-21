4658c6845026156782e2ad26d2dbf5f0789105e5
Y.KEISUKE <yamagutikeisuke@gmail.com>
2018-05-21 23:10:06 +0900

    Initial Commit

---
+++ CoreData-sample2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreData-sample2
+//
+//  Created by Keisuke Yamaguchi on 2018/05/21.
+//  Copyright © 2018年 Keisuke Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
